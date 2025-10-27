b97df3f6ac44017d8c8d5ef504f0b41d1781be92
starday119 <jy8294@naver.com>
2025-10-27 11:35:04 +0900

    test(RacingGameTest): 시도 횟수 예외 처리 테스트 추가

--- src/test/java/racingcar/RacingGameTest.java
+++ src/test/java/racingcar/RacingGameTest.java
@@ -1,11 +1,13 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.error.ErrorMessage;
 import racingcar.model.Cars;
 import racingcar.model.RacingGame;
 
@@ -25,5 +27,16 @@ public class RacingGameTest {
         // then
         assertThat(results).isEmpty();
     }
+    @Test
+    @DisplayName("시도 횟수가 음수이면 예외가 발생한다")
+    void playWithNegativeAttempts() {
+        // given
+        List<String> names = Arrays.asList("pobi", "crong");
+        Cars cars = Cars.from(names);
+        int tryCount = -1;
 
+        // when & then
+        assertThatThrownBy(() -> new RacingGame(cars, tryCount))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
